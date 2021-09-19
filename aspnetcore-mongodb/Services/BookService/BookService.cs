5ea3d5e865a2941b6e08a404d46d00e6e9632c43
Samet UCA <Samet UCA@DESKTOP-CME968B>
2021-09-19 15:10:26 +0300

    Initial commit

---
+++ aspnetcore-mongodb/Services/BookService/BookService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using aspnetcore_mongodb.Models;
+using aspnetcore_mongodb.Models.MongoSettings;
+using MongoDB.Driver;
+
+namespace aspnetcore_mongodb.Services.BookService
+{
+    public class BookService
+    {
+        private readonly IMongoCollection<Book> _books;
+
+        public BookService(IBookstoreDatabaseSettings settings)
+        {
+
+            var client = new MongoClient(settings.ConnectionString);
+
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _books = database.GetCollection<Book>(settings.BooksCollectionName);
+
+        }
+
+        public List<Book> Get()
+        {
+            var findingBooks = _books.Find(book => true).ToList();
+            return findingBooks;
+        }
+        public Book Get(string id) =>
+            _books.Find<Book>(book => book.Id == id).FirstOrDefault();
+
+        public Book Create(Book book)
+        {
+            _books.InsertOne(book);
+            return book;
+        }
+
+        public void Update(string id, Book bookIn) =>
+            _books.ReplaceOne(book => book.Id == id, bookIn);
+
+        public void Remove(Book bookIn) =>
+            _books.DeleteOne(book => book.Id == bookIn.Id);
+
+        public void Remove(string id) =>
+            _books.DeleteOne(book => book.Id == id);
+
+    }
+}
